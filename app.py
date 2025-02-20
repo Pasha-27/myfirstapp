026c355e39031caff978d460ef0bce19c12fbff7
Pasha-27 <akshay.parashar@learnapp.com>
2025-02-20 16:48:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,15 @@ from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, No
 import openai
 
 # OpenAI API Key (Replace with your actual key)
-#OPENAI_API_KEY = "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"
 #openai.api_key = OPENAI_API_KEY
 #client = openai.api_key
 #client = OpenAI(api_key=OPENAI_API_KEY)
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 YOUTUBE_API_KEY = st.secrets["YOUTUBE_API_KEY"]
 
-client = openai.OpenAI(api_key="OPENAI_API_KEY")
+client = openai.OpenAI(api_key=OPENAI_API_KEY)
+#client = openai.Client(api_key=OPENAI_API_KEY)
 
-
-# YouTube API Key (Replace with your actual API key)
-YOUTUBE_API_KEY = "AIzaSyBoDd0TbpH0-NehCVi_QHc4p_lKmjCeIyY"
 
 # Initialize YouTube API
 def get_youtube_service():
